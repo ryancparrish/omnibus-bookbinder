10ae90323541145105cd4dce090b2ad897ee179e
Ryan Parrish <rparrish@pivotal.io>
2015-01-20 13:35:46 -0500

    co-located wrapper app

--- config/software/bookbinder.rb
+++ config/software/bookbinder.rb
@@ -1,7 +1,9 @@
 name "bookbinder"
 
-source git: "https://github.com/ryancparrish/bookbinder-wrapper"
-default_version "master"
+#source git: "https://github.com/ryancparrish/bookbinder-wrapper"
+#default_version "master"
+source_path = "#{Omnibus::Config.project_root}/resources/bookbinder/wrapper-app"
+source path: source_path
 
 dependency "ruby"
 dependency "rubygems"
@@ -11,11 +13,19 @@ dependency "sqlite3"   # have omnibus bundle sqlite3 native libs
 
 build do
   env = with_standard_compiler_flags(with_embedded_path)
+
+  # Budnle the wrapper app dependencies
   bundle "install"
-  copy("#{Omnibus::Config.project_root}/bookbinder-scripts/*", "#{install_dir}/bin")
-#  copy("#{Omnibus::Config.project_root}/bookbinder-scripts/Gemfile*", "#{install_dir}/bin")
-#  erb source: 'bookbinder.erb',
-#      dest:   "#{install_dir}/bin",
-#      vars:   { install_dir: "#{install_dir}" },
-#      mode:   '0755'
+
+  # Make sure the wrapper app is placed in the bin dir of the installer package.
+  copy "#{source_path}/*", "#{install_dir}/bin"
+
+  # Hardcopied the ruby delcaration in the script, since the omnibus templating failed.
+  #  erb source: 'bookbinder.erb',
+  #      dest:   "#{install_dir}/bin",
+  #      vars:   { install_dir: "#{install_dir}" },
+  #      mode:   '0755'
+
+  command "chmod +x #{install_dir}/bin/bookbinder"
+
 end
