fa6e706f58a3194c9aa7170de8e11904c84814e4
Ryan Parrish <rparrish@pivotal.io>
2015-01-27 13:05:56 -0500

    Added env wrapper script, additional commands, and pkg hook placeholders

---
+++ resources/bookbinder/wrapper-app/bookbinder-wrapper.rb
@@ -0,0 +1,29 @@
+require 'bookbinder'
+require 'optparse'
+
+# The `bookbinder --version` command only works if running a locally bundled gem,
+# not if the gem was installed via rubygems.
+# For this reason, we're intercepting the version command and reporting the gem
+# version.
+def print_version
+  gem_spec = Gem::Specification.find_by_name('bookbindery')
+  puts "Bookbinder #{gem_spec.version}"
+end
+
+def run_bookbinder
+  # Disabling HTTPS certificate verification to avoid these exceptions
+  # when bookbinder is verifying connectivity to external resources
+  # (e.g. css links).  Tried updating embedded openssl, but verification
+  # still failed with latest cacert.pem.
+  OpenSSL::SSL.const_set(:VERIFY_PEER, OpenSSL::SSL::VERIFY_NONE)
+
+  return_code = Bookbinder::Cli.new.run ARGV
+  exit return_code.to_i
+end
+
+OptionParser.new do |o|
+  o.on('--version') { print_version; exit 0 }
+  o.parse!
+end
+
+run_bookbinder
